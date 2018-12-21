0f58c9e2ea4dd78c337c5908047297ac5635227a
francybiga <francybiga@gmail.com>
2018-12-21 15:05:11 +0100

    Git repo struct

--- Sources/PodspecUtilsCore/PodspecUtils.swift
+++ Sources/PodspecUtilsCore/PodspecUtils.swift
@@ -12,7 +12,9 @@ public final class PodspecUtils {
         //CLI
         let utilsCli = CLI(name: "PodspecUtils", version: "0.0.1", description: "Automate interaction with Cocoapods' podspecs files.")
         
+//        let repo = GitRepo(path: )
         let shell = Shell()
+        
         do {
             print(try shell.execute("pwd", in: "~"))
         } catch ShellError.standard(let msg) {
@@ -23,9 +25,19 @@ public final class PodspecUtils {
     }
 }
 
-//Git
-extension PodspecUtils {
-    func isRepoDirty() {
+struct GitRepo {
+    private let repoPath: String
+    private let shell = Shell()
     
+    init(path: String) {
+        self.repoPath = path
+    }
+    
+    private func exec(command: String) throws -> String {
+        return try shell.execute(command, in: repoPath)
+    }
+    
+    func gitStatusClean() -> Bool {
+        return try! exec(command: "git status --porcelain | wc -l").trimmingCharacters(in: .whitespacesAndNewlines) == "0"
     }
 }
