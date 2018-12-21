273134cda06530834e6191842e3a6f6931b8e15d
francybiga <francybiga@gmail.com>
2018-12-21 09:15:43 +0100

    Shell class

---
+++ Sources/PodspecUtilsCore/Shell.swift
@@ -0,0 +1,45 @@
+//
+//  Shell.swift
+//  PodspecUtilsCore
+//
+//  Created by Francesco Bigagnoli on 21/12/2018.
+//
+
+import Foundation
+
+enum ShellError: Error {
+    case standard(String)
+}
+
+final class Shell {
+    private let path: String
+    
+    init(path: String = "/bin/sh") {
+        self.path = path
+    }
+    
+    func execute(_ command: String) throws -> String {
+        let task = Process()
+        task.arguments = ["-c", command]
+        task.launchPath = path
+        
+        let outPipe = Pipe()
+        let errPipe = Pipe()
+        task.standardOutput = outPipe
+        task.standardError = errPipe
+        
+        task.launch()
+        
+        let errData = errPipe.fileHandleForReading.readDataToEndOfFile()
+        guard errData.isEmpty else {
+            let errorString = String(data: errData, encoding: .utf8)!
+            let error = ShellError.standard("⚠️ " + errorString)
+            throw error
+        }
+        
+        let outData = outPipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: outData, encoding: .utf8)!
+        
+        return output
+    }
+}
